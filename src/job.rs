75e9dae9098b087871a7380d5b015387ff2c9687
Mark Sherry <mdsherry@gmail.com>
2019-12-24 21:54:12 -0500

    Remove Sync from requirements for a job (#8)
    Thanks to chino540off for realizing it's unnecessary

--- src/job.rs
+++ src/job.rs
@@ -16,7 +16,7 @@ where
     frequency: Vec<RunConfig>,
     next_run: Option<DateTime<Tz>>,
     last_run: Option<DateTime<Tz>>,
-    job: Option<Box<dyn FnMut() + Sync + Send>>,
+    job: Option<Box<dyn FnMut() + Send>>,
     tz: Tz,
     _tp: PhantomData<Tp>,
 }
@@ -105,7 +105,7 @@ where
     /// Specify a task to run, and schedule its next run
     pub fn run<F>(&mut self, f: F) -> &mut Self
     where
-        F: 'static + FnMut() + Sync + Send,
+        F: 'static + FnMut() + Send,
     {
         self.job = Some(Box::new(f));
         match self.next_run {
